f65682fd6e465ec0dccb5a8d2798d321efccbd4f
kalinalazarova1 <kalina.lazarova@gmail.com>
2019-02-10 14:10:48 +0000

    changes

--- BookingSystem/Controllers/SitesController.cs
+++ BookingSystem/Controllers/SitesController.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using BookingSystem.Data;
+using BookingSystem.Models.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookingSystem.Controllers
 {
     [Produces("application/json")]
     [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class SitesController : Controller
     {
         BookingContext ctx;
@@ -21,11 +20,69 @@ namespace BookingSystem.Controllers
             this.ctx = ctx;
         }
 
-        [HttpGet, Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet]
         public IActionResult Get()
         {
             var sites = ctx.Sites.ToList();
             return Ok(sites);
+        }
+
+        [HttpGet("{id}", Name = "SiteGet")]
+        public IActionResult Get(int id)
+        {
+            var site = ctx.Sites.FirstOrDefault(s => s.SiteId == id);
+            return Ok(site);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]Site model)
+        {
+            this.ctx.Add(model);
+            if (await this.ctx.SaveChangesAsync() > 0)
+            {
+                var url = Url.Link("SiteGet", new { id = model.SiteId });
+                return Created(url, model);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody]Site model)
+        {
+            var oldSite = this.ctx.Sites.FirstOrDefault(s => s.SiteId == id);
+            if(oldSite == null)
+            {
+                return NotFound();
+            }
+
+            oldSite.OwnerId = model.OwnerId ?? oldSite.OwnerId;
+            oldSite.Location = model.Location ?? oldSite.Location;
+            this.ctx.Add(oldSite);
+
+            if (await this.ctx.SaveChangesAsync() > 0)
+            {
+                return Ok(oldSite);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var site = this.ctx.Sites.FirstOrDefault(s => s.SiteId == id);
+            if (site == null)
+            {
+                return NotFound();
+            }
+
+            if (await this.ctx.SaveChangesAsync() > 0)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
         }
     }
 }
